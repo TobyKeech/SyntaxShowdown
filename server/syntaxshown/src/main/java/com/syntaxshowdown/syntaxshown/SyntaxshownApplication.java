96fb8bddc7ea6f78646cc8a6fc8e8098fd413242
Toby Keech <toby.keech18@gmail.com>
2023-06-05 13:00:24 +0100

    added spring application to server folder

---
+++ server/syntaxshown/src/main/java/com/syntaxshowdown/syntaxshown/SyntaxshownApplication.java
@@ -0,0 +1,13 @@
+package com.syntaxshowdown.syntaxshown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntaxshownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntaxshownApplication.class, args);
+	}
+
+}
---
+++ server/syntaxshown/src/test/java/com/syntaxshowdown/syntaxshown/SyntaxshownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntaxshowdown.syntaxshown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntaxshownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
