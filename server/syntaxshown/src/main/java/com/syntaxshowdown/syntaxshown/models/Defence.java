cf07634d1bf0416d9e88d4415e443dbcc20a8f29
Toby Keech <toby.keech18@gmail.com>
2023-06-05 14:24:47 +0100

    added all repos and id/colums

--- server/syntaxshown/src/main/java/com/syntaxshowdown/syntaxshown/models/Defence.java
+++ server/syntaxshown/src/main/java/com/syntaxshowdown/syntaxshown/models/Defence.java
@@ -1,12 +1,21 @@
 package com.syntaxshowdown.syntaxshown.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "defences")
 public enum Defence {
+
+    @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     JSONIGNORE(10),
     GITPUSH(20),
     GUARD(20),
     FETCH(20);
 
+    private Long id;
+    @Column(name = "defencevalue")
     private int defencevalue;
 
     Defence(int defencevalue) {
