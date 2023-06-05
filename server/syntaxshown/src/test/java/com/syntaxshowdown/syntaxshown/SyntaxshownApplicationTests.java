2c3e910fb67ab57e74d46b7b5d3d4fc25823731e
Toby Keech <toby.keech18@gmail.com>
2023-06-05 14:57:38 +0100

    added relationships to models

--- server/syntaxshown/src/test/java/com/syntaxshowdown/syntaxshown/SyntaxshownApplicationTests.java
+++ server/syntaxshown/src/test/java/com/syntaxshowdown/syntaxshown/SyntaxshownApplicationTests.java
@@ -1,13 +1,31 @@
 package com.syntaxshowdown.syntaxshown;
 
+import com.syntaxshowdown.syntaxshown.repositories.AttackRepository;
+import com.syntaxshowdown.syntaxshown.repositories.CharacterRepository;
+import com.syntaxshowdown.syntaxshown.repositories.DefenceRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SyntaxshownApplicationTests {
 
+	@Autowired
+	CharacterRepository characterRepository;
+
+	@Autowired
+	AttackRepository attackRepository;
+
+	@Autowired
+	DefenceRepository defenceRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void canSaveCharacter(){
+
 	}
 
 }
