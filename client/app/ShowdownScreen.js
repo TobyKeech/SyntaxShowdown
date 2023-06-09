b5b4f7dbf9ac548c29287a98bbb214e24af20816
Toby Keech <toby.keech18@gmail.com>
2023-06-09 14:01:41 +0100

    added changes to info display images/hp/name

--- client/app/ShowdownScreen.js
+++ client/app/ShowdownScreen.js
@@ -149,7 +149,7 @@ const ShowdownScreen = () => {
             alignItems: "center",
             width: overlayWidth,
             height: overlayHeight,
-            backgroundColor: "rgb(0,0,0)",
+            backgroundColor: "bg-opacity-50",
           }}
           isVisible={abilityvisiblep1}
           animationType="fade"
@@ -192,7 +192,7 @@ const ShowdownScreen = () => {
             alignItems: "center",
             width: overlayWidth,
             height: overlayHeight,
-            backgroundColor: "rgb(0,0,0)",
+            backgroundColor: "bg-opacity-50",
           }}
           isVisible={abilityvisiblep2}
           animationType="fade"
@@ -315,7 +315,7 @@ const ShowdownScreen = () => {
           </Overlay>
         ) : null}
 
-        <View className="flex-row">
+        <View style="flex: 1; justify-content: flex-end" className="flex-row">
           {characterData ? <Player character={characterData} /> : null}
           {secondCharacterData ? (
             <Player character={secondCharacterData} />
