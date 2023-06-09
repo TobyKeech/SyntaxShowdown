6daa14d2f183f7364619747b46c04a5b89dce4f1
Toby Keech <toby.keech18@gmail.com>
2023-06-09 14:44:16 +0100

    added styling to hp

--- client/components/PlayerBox/PlayerHP.js
+++ client/components/PlayerBox/PlayerHP.js
@@ -2,10 +2,13 @@ import { View, Text } from "react-native";
 import React from "react";
 
 const PlayerHP = ({ characterhp }) => {
+
   return (
-   <View >
+   <View style={{ borderWidth: 2, borderColor: 'white', width: '100%', height: 30, backgroundColor: 'rgb(74 222 128)' }}>
       <Text className=" text-6xl text-green-400">{characterhp.hp}</Text>
     </View>
+
+  
   );
 };
 
